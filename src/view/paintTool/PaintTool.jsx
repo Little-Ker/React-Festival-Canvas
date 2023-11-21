688975ea27392b97eb0d62900667406c4ec56c88
Vivi <vivichi@yile.com.tw>
2023-11-21 17:11:10 +0800

    [新增] 設定工具

--- src/view/paintTool/PaintTool.jsx
+++ src/view/paintTool/PaintTool.jsx
@@ -1,5 +1,5 @@
 import React, {
-  useState, useCallback, useEffect, useMemo, useRef
+  useState, useCallback, useEffect
 } from 'react'
 import styles from './paintTool.module.sass'
 import IconButton from '@mui/material/IconButton'
@@ -10,13 +10,17 @@ import CodeIcon from '@mui/icons-material/Code'
 import HighlightOffIcon from '@mui/icons-material/HighlightOff'
 import SettingsIcon from '@mui/icons-material/Settings'
 import {
-  openAlert, openConfirm, closeDialogs  
+  openAlert, openConfirm, closeDialogs, openForm
 } from 'component/dialog'
 import CallMissedIcon from '@mui/icons-material/CallMissed'
 import MenuList from '@mui/material/MenuList'
 import MenuItem from '@mui/material/MenuItem'
 import Divider from '@mui/material/Divider'
+import SettingView from './sub/settingView'
 import clsx from 'clsx'
+
+const penIndex = 0
+const settingIndex = 1
 
 function PaintTool() {
   const [canvas, setCanvas] = useState(null)
@@ -25,17 +29,13 @@ function PaintTool() {
   const [useTool, setUseTool] = useState([false, false])
 
   const [setting, setSetting] = useState({
-    gridSize: 250,
-    FPS: 50,
     isShowGrid: true,
+    gridSize: 250,
   })
 
+  // 鋼筆工具
   const [storyPointAry, setStoryPointAry] = useState([])
   const [storyPointCount,  setStoryPointCount] = useState(0)
-
-  const globalControl = {
-    storyPointAry: [],
-  }
 
   const resizeCanvas = useCallback(() => {
     let ww = window.innerWidth - 3
@@ -49,8 +49,8 @@ function PaintTool() {
     resizeCanvas()
   })
 
-  const clearArray = useCallback(() => {
-    openConfirm('確認是否清除畫布?', () => {
+  const onResetCanvas = useCallback(() => {
+    openConfirm('確認', '是否清除畫布?', () => {
       setStoryPointAry((prev) => {
         for(let i = 0; i < storyPointCount; i++) {
           prev.pop()
@@ -70,7 +70,7 @@ function PaintTool() {
     setStoryPointCount((prev) => {
       return prev - 1
     })
-  }, [globalControl.storyPointAry])
+  }, [])
 
   const onOpenPointCode = useCallback(() => {
     openAlert(
@@ -104,22 +104,42 @@ function PaintTool() {
     })
   }, [ctx, storyPointAry])
 
-  const onMouseDown = useCallback((evt) => {
-    setStoryPointAry((prev) => {
-      prev.push({
-        x: evt.x,
-        y: evt.y,
-      })
-      return prev
-    })
+  const drawGrid = useCallback(() => {
+    let ww = window.innerWidth - 3
+    let wh = window.innerHeight - 3
 
-    setStoryPointCount(prev => prev + 1)
+    if (!setting.isShowGrid) return
+    ctx.strokeStyle='rgba(255,255,255,0.4)'
+    const gridWidth = setting.gridSize
+    const gCount = ww / gridWidth
+    ctx.beginPath()
+    for(let i = 0; i <= gCount; i++) {
+      ctx.moveTo(i * gridWidth, -wh)
+      ctx.lineTo(i * gridWidth, wh)
+      ctx.moveTo(-ww, i * gridWidth)
+      ctx.lineTo(ww, i * gridWidth)
+    }
+    ctx.strokeStyle='rgba(255,255,255,0.4)'
+    ctx.stroke()
+  }, [ctx, setting])
+
+  const onMouseDown = useCallback((evt) => {
+    if (useTool[penIndex]) {
+      setStoryPointAry((prev) => {
+        prev.push({
+          x: evt.x,
+          y: evt.y,
+        })
+        return prev
+      })
+      setStoryPointCount(prev => prev + 1)
+    }
   }, [useTool])
 
   useEffect(() => {
     if (!canvas) return
     canvas.addEventListener('mousedown', onMouseDown)
-
+    return () => canvas.removeEventListener('mousedown', onMouseDown)
     // canvas.addEventListener('mousemove', (evt) => {})
     // canvas.addEventListener('mouseup', (evt) => {})
   }, [onMouseDown])
@@ -139,29 +159,23 @@ function PaintTool() {
     ctx.fillRect(0,0,ww,wh)
 
     // 繪製網格背景
-    ctx.strokeStyle='rgba(255,255,255,0.4)'
-    const gridWidth = setting.gridSize
-    const gCount = ww / gridWidth
-    ctx.beginPath()
-    for(let i = 0; i <= gCount; i++) {
-      ctx.moveTo(i * gridWidth, -wh)
-      ctx.lineTo(i * gridWidth, wh)
-      ctx.moveTo(-ww, i * gridWidth)
-      ctx.lineTo(ww, i * gridWidth)
-    }
-    ctx.strokeStyle='rgba(255,255,255,0.4)'
-    ctx.stroke()
+    drawGrid()
 
     // 鋼筆工具效果
     drawPoint()
-  }, [drawPoint, globalControl.storyPointAry])
+  }, [drawPoint])
+
+  const onCancel = useCallback(() => {
+    const useToolAry = useTool.map(() => false)
+    setUseTool(useToolAry)
+    closeDialogs()
+    paint()
+  }, [useTool, setting, paint])
 
   useEffect(() => {
     if (!ctx) return
     resizeCanvas()
-
-    setInterval(paint, setting.FPS)
-    // paint()
+    paint()
   }, [ctx])
 
   const initCanvas = useCallback(() => {
@@ -180,7 +194,19 @@ function PaintTool() {
     const useToolAry = useTool.map(() => false)
     useToolAry[index] = true
     setUseTool(useToolAry)
-  }, [useTool])
+
+    if (settingIndex === index) {
+      openForm({
+        title: '設定',
+        content: (
+          <SettingView
+            setting={setting}
+            setSetting={setSetting}
+            onCancel={onCancel}
+          />),
+      })
+    }
+  }, [useTool, setting, setSetting, onCancel])
 
   useEffect(() => {
     closeDialogs()
@@ -192,7 +218,7 @@ function PaintTool() {
       <div className={styles.toolBox}>
         <div className={styles.tool}>
           <Tooltip title={'鋼筆'} placement="right">
-            <IconButton size="medium" onClick={() => onHandleClick(0)} className={clsx(useTool[0] && styles.use)}>
+            <IconButton size="medium" onClick={() => onHandleClick(penIndex)} className={clsx(useTool[penIndex] && styles.use)}>
               <LinearScaleIcon />
             </IconButton >
           </Tooltip>
@@ -206,7 +232,7 @@ function PaintTool() {
                 <CodeIcon fontSize="small" />
                 <p className={styles.btnText}>{'轉成程式碼'}</p>
               </MenuItem>
-              <MenuItem onClick={clearArray} className={styles.toolBtn}>
+              <MenuItem onClick={onResetCanvas} className={styles.toolBtn}>
                 <HighlightOffIcon fontSize="small" />
                 <p className={styles.btnText}>{'清除畫面'}</p>
               </MenuItem>
@@ -216,7 +242,7 @@ function PaintTool() {
         </div>
         <div className={styles.tool}>
           <Tooltip title={'設定'} placement="right">
-            <IconButton size="medium"onClick={() => onHandleClick(1)} className={clsx(useTool[1] && styles.use)}>
+            <IconButton size="medium"onClick={() => onHandleClick(settingIndex)} className={clsx(useTool[settingIndex] && styles.use)}>
               <SettingsIcon />
             </IconButton >
           </Tooltip>
---
+++ src/view/paintTool/sub/settingView/SettingView.jsx
@@ -0,0 +1,85 @@
+import React, {
+  useState, useCallback
+} from 'react'
+import PropTypes from 'prop-types'
+import {
+  TextField, Checkbox , FormControlLabel, Box, Button, Grid, Divider
+} from '@mui/material'
+import {
+  openAlert, openConfirm, closeDialogs, openForm
+} from 'component/dialog'
+
+function SettingView(props) {
+  const {setting, setSetting, onCancel} = props
+
+  const [isShowGrid, setIsShowGrid] = useState(setting.isShowGrid)
+  const [gridSize, setGridSize] = useState(setting.gridSize)
+
+  const onSend = useCallback(() => {
+    if (isShowGrid && gridSize < 1) {
+      openAlert(
+        {title: '提示',
+          content: '格線大小不可小於 0'}
+      )
+      return 
+    }
+  
+    setSetting((prev) => {
+      prev.isShowGrid = isShowGrid
+      prev.gridSize = Number(gridSize)
+      return prev
+    })
+    onCancel()
+  }, [isShowGrid, gridSize])
+
+  return (
+    <Box sx={{padding: '1rem'}}>
+      <Grid
+        container
+        spacing={2}
+        direction="column"
+      >
+        <Grid item>
+          <FormControlLabel
+            control={(
+              <Checkbox
+                checked={isShowGrid}
+                onChange={event => setIsShowGrid(event.target.checked)}
+              />
+            )}
+            label={<p>{'開啟格線工具'}</p>}
+            labelPlacement="start"
+          />
+          <TextField
+            label={'格線大小'}
+            value={gridSize}
+            onChange={(event) => {
+              setGridSize(event.target.value)
+            }}
+            variant="standard"
+            inputProps={{
+              type: 'number',
+            }}
+            sx={{marginLeft: '1rem'}}
+          />
+        </Grid>
+        <Divider sx={{marginTop: '1rem'}} />
+        <Grid item
+          sx={{display: 'flex',
+            justifyContent: 'flex-end'}}
+        >
+          <Button sx={{marginRight: '.5rem'}} onClick={onCancel} variant="outlined">{'取消'}</Button>
+          <Button variant="contained" onClick={onSend}>{'確認修改'}</Button>
+        </Grid>
+      </Grid>
+    </Box>
+  )
+}
+
+SettingView.propTypes = {
+  setting: PropTypes.object,
+  setSetting: PropTypes.func,
+  onCancel: PropTypes.func,
+}
+
+export default SettingView
